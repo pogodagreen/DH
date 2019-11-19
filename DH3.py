7dbb707a32cedf4cbe73ab81752eb23534fc3f64
JPogodzinski <pogodzinskij@gmail.com>
2019-11-19 12:15:00 +0100

    works for 3 people

---
+++ DH3.py
@@ -0,0 +1,82 @@
+import random
+
+
+def isPrime(n):
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def GCD(x, y):
+    while y != 0:
+        pom = y
+        y = x % y
+        x = pom
+    return x
+
+
+def isCoPrime(x, y):
+    if GCD(x, y) == 1:
+        return True
+    return False
+
+def primRoots(modulo):
+    roots = []
+    required_set = set(num for num in range (1, modulo) if GCD(num, modulo) == 1)
+
+    for g in range(1, modulo):
+        actual_set = set(pow(g, powers) % modulo for powers in range (1, modulo))
+        if required_set == actual_set:
+            roots.append(g)
+    return roots
+
+
+if __name__ == '__main__':
+    n = random.randrange(10, 100)
+    while not isPrime(n):
+        n = random.randrange(10, 100)
+    print ("n", n)
+    print(primRoots(n))
+    g=max(primRoots(n))
+    print(g)
+
+    #A
+    x = random.randrange(10, 100)
+    while not isPrime(n):
+        x = random.randrange(10, 100)
+    X=g**x %n
+
+    #B
+    y = random.randrange(10, 100)
+    while not isPrime(n):
+        y = random.randrange(10, 100)
+    Y = g ** y % n
+
+    #C
+    z = random.randrange(10, 100)
+    while not isPrime(n):
+        z = random.randrange(10, 100)
+    Z = g ** z % n
+
+    #A
+    Z1=Z**x %n
+
+    #B
+    X1=X**y %n
+
+    #C
+    Y1=Y**z%n
+
+    #A
+    k1=Y1**x%n
+
+    #B
+    k2 = Z1 ** y % n
+
+    #C
+    k3 = X1 ** z % n
+
+    if(k1==k2==k3):
+        print("It's the same")
+    else:
+        print ("They're not the same")
