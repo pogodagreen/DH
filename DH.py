d66cee00b21e9722b15448c37a8165c96a197167
JPogodzinski <pogodzinskij@gmail.com>
2019-11-19 11:56:07 +0100

    added gcd and isCoPrime funs

--- DH.py
+++ DH.py
@@ -1,5 +1,30 @@
+import random
+
+
 def isPrime(n):
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
+
+def GCD(x, y):
+    while y != 0:
+        pom = y
+        y = x % y
+        x = pom
+    return x
+
+
+def isCoPrime(x, y):
+    if GCD(x, y) == 1:
+        return True
+    return False
+
+def primRoots(modulo):
+    
+
+if __name__ == '__main__':
+    n = random.randrange(1000, 10000)
+    while not isPrime(n):
+        n = random.randrange(1000, 10000)
+    print ("n", n)
